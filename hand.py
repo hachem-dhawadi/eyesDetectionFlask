5c014d0ad9cfaac0d9bac961bc556b1dab30547a
hachem-dhawadi <hachemdhawadi2001@gmail.com>
2025-03-07 17:20:55 +0000

    hand detection

--- hand.py
+++ hand.py
@@ -97,11 +97,9 @@ def generate_frames():
 def index():
     return render_template('index.html')
 
-
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
